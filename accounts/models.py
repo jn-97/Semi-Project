df35bf5d9793aa5a6d4a4d851d0cee488eed5e59
OIIUOI <eunjin4788@gmail.com>
2022-11-01 14:17:45 +0900

    User

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
+
+class User(AbstractUser):
+    profile = ProcessedImageField(upload_to='images/', blank=True,
+                                processors=[ResizeToFill(1200, 960)],
+                                format='JPEG',
+                                options={'quality': 80})
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    
---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('login/', views.log_in, name='login'),
+    path('logout/', views.log_out, name='logout'),
+]

4959927fc0953e16360d30962c2076f82449e893
lsb0709 <dltkdqor0709@gmail.com>
2022-11-03 11:10:12 +0900

    restaurants/index

---
+++ static/js/asd.js
@@ -0,0 +1,47 @@
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,  //초기값 설정 모바일값이 먼저!!
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+    
+      768: {
+        slidesPerView: 2,  //브라우저가 768보다 클 때
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,  //브라우저가 1024보다 클 때
+        spaceBetween: 50,
+      },
+    },
+    autoplay:{
+        delay: 10000,
+    }
+  });
+
+  $(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+
+  $( document ).ready(function () {
+    $(".moreBox").slice(0, 3).show();
+      if ($(".blogBox:hidden").length != 0) {
+        $("#loadMore").show();
+      }   
+      $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $(".moreBox:hidden").slice(0, 6).slideDown();
+        if ($(".moreBox:hidden").length == 0) {
+          $("#loadMore").fadeOut('slow');
+        }
+      });
+    });

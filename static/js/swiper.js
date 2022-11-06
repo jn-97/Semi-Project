bfc498e3b558cd02a1595a74ef81d0bf5d935899
lsb0709 <dltkdqor0709@gmail.com>
2022-11-06 17:51:48 +0900

    ??

---
+++ static/js/swiper.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,  //초기값 설정 모바일값이 먼저!!
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+    
+      768: {
+        slidesPerView: 2,  //브라우저가 768보다 클 때
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,  //브라우저가 1024보다 클 때
+        spaceBetween: 50,
+      },
+    },
+    autoplay:{
+        delay: 10000,
+    }
+  });
+
+  $(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+
+
